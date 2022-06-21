e85b76cd1c4c9dbd165050ad28e0fae83fd4a480
shhady <shhadyse@gmail.com>
2022-06-21 13:01:30 +0300

    add

---
+++ 13_1Blog/blog13_1.js
@@ -0,0 +1,10 @@
+const mongo = require("mongo");
+// to create two collections -
+// 1. posts -
+// 2. users-
+
+// in the posts collection two properties post,
+//and comments (array[]) and to save the user id in the comments
+
+// let the user make a post .
+// and let him leave a comment
